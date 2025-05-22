8f2dc0c31ad946b1f2fe34ae92a54b3ac5e96343
Sören Stöhrmann <soeren.stoehrmann@mb-satellite.com>
2025-05-22 16:05:24 +0200

    Fixes error

--- git-things/build-patch.sh
+++ git-things/build-patch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/6658313/how-can-i-generate-a-git-patch-for-a-specific-commit
 
-git format-patch <numberofcommits> HEAD
+git format-patch -<numberofcommits> HEAD
